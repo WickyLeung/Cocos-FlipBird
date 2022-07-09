ebfd6fe77c91bb0a2c84b5cc7f4a2d52851c3887
Wicky <489079371@qq.com>
2022-07-09 21:59:31 +0800

    重新设置好约束

---
+++ temp/programming/packer-driver/targets/preview/mods/fs/0/assets/Scripts/bird.js
@@ -0,0 +1,97 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, _decorator, Component, Animation, _dec, _dec2, _class, _class2, _descriptor, _temp, _crd, ccclass, property, bird;
+
+  function _initializerDefineProperty(target, property, descriptor, context) { if (!descriptor) return; Object.defineProperty(target, property, { enumerable: descriptor.enumerable, configurable: descriptor.configurable, writable: descriptor.writable, value: descriptor.initializer ? descriptor.initializer.call(context) : void 0 }); }
+
+  function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+  function _initializerWarningHelper(descriptor, context) { throw new Error('Decorating class property failed. Please ensure that ' + 'proposal-class-properties is enabled and runs after the decorators transform.'); }
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+      Animation = _cc.Animation;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "77041WPPChLFKoWeRzjyjUE", "bird", undefined);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+      /**
+       * Predefined variables
+       * Name = bird
+       * DateTime = Wed Jul 06 2022 18:32:08 GMT+0800 (中国标准时间)
+       * Author = Wicky_Leung
+       * FileBasename = bird.ts
+       * FileBasenameNoExtension = bird
+       * URL = db://assets/Scripts/bird.ts
+       * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+       *
+       */
+
+      _export("bird", bird = (_dec = ccclass('bird'), _dec2 = property({
+        type: Animation
+      }), _dec(_class = (_class2 = (_temp = class bird extends Component {
+        constructor() {
+          super(...arguments);
+
+          _initializerDefineProperty(this, "anim", _descriptor, this);
+
+          _defineProperty(this, "flappingAnim", null);
+        }
+
+        onLoad() {
+          this.flappingAnim = this.getComponent(Animation);
+        }
+
+        start() {
+          // [3]
+          if (this.flappingAnim) {
+            this.flappingAnim.play('birdFlapping');
+          }
+
+          if (this.anim) {
+            this.anim.play("birdWing");
+          }
+        } // update (deltaTime: number) {
+        //     // [4]
+        // }
+
+
+      }, _temp), (_descriptor = _applyDecoratedDescriptor(_class2.prototype, "anim", [_dec2], {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        initializer: function initializer() {
+          return null;
+        }
+      })), _class2)) || _class));
+      /**
+       * [1] Class member could be defined like this.
+       * [2] Use `property` decorator if your want the member to be serializable.
+       * [3] Your initialization goes here.
+       * [4] Your update function goes here.
+       *
+       * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/en/scripting/
+       * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/en/scripting/decorator.html
+       * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/en/scripting/life-cycle-callbacks.html
+       */
+
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=bird.js.map
